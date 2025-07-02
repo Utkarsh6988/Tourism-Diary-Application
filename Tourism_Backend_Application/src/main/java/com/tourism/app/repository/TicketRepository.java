7e0c58b665c421e96fbecc8e4addf9ea2a7f5077
Utkarsh5669 <utkarsharora6387555198@gmail.com>
2025-07-03 01:25:48 +0530

    Remove all directories except Tourism_Backend_Application

--- Tourism_Backend_Application/src/main/java/com/tourism/app/repository/TicketRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tourism.app.repository;
-
-import com.tourism.app.model.Ticket;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketRepository extends JpaRepository<Ticket, Long> {
-}
--- Tourism_Backend_Application/src/main/java/com/tourism/app/service/TicketService.java
+++
@@ -1,8 +0,0 @@
-package com.tourism.app.service;
-
-
-import com.tourism.app.model.Ticket;
-
-public interface TicketService {
-    Ticket bookTicket(Ticket ticket);
-}
